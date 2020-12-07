ca2e0a07ed3728b92b44d4fece0e5594e3b4f736
rod-glover <rod.glover.ca@gmail.com>
2020-12-06 17:02:19 -0800

    Add representation

---
+++ cf_cell_methods/representation.py
@@ -0,0 +1,86 @@
+import re
+
+
+def eq(a, b, attrs):
+    """Compare two objects on named attributes"""
+    for attr in re.split(r"\s*,\s*", attrs):
+        left, right = (getattr(obj, attr) for obj in (a, b))
+        if left != right:
+            return False
+    return True
+
+
+def strict_join(seq, sep=" "):
+    """Join a sequence of objects "strictly", which means ignoring None
+    values."""
+    return sep.join(s for s in seq if s is not None)
+
+
+class CellMethod:
+    def __init__(self, name, method, where=None, over=None, extra_info=None):
+        self.name = name
+        self.method = method
+        self.where = where
+        self.over = over
+        self.extra_info = extra_info
+
+    def __eq__(*args):
+        return eq(*args, "name, method, where, over, extra_info")
+
+    def __str__(self):
+        return strict_join(
+            (
+                f"{self.name}: {self.method}",
+                self.where and f"where {self.where}",
+                self.over and f"over {self.over}",
+                self.extra_info and f"{self.extra_info}",
+            )
+        )
+
+
+class ExtraInfo:
+    def __init__(self, standardized, non_standardized):
+        self.standardized = standardized
+        self.non_standardized = non_standardized
+
+    def __eq__(*args):
+        return eq(*args, "standardized, non_standardized")
+
+    def __str__(self):
+        if self.standardized is None and self.non_standardized is None:
+            # This actually should never occur
+            return ""
+        return (
+            f"("
+            f"{self.standardized or ''}"
+            f"{' comment: ' if self.standardized and self.non_standardized else ''}"
+            f"{self.non_standardized or ''}"
+            f")"
+        )
+
+
+class StandardizedExtraInfo:
+    pass
+
+
+class SxiInterval(StandardizedExtraInfo):
+    def __init__(self, value, unit):
+        self.value = value
+        self.unit = unit
+
+    def __eq__(*args):
+        return eq(*args, "value, unit")
+
+    def __str__(self):
+        return f"interval: {self.value} {self.unit}"
+
+
+class Percentile:
+    def __init__(self, value):
+        self.value = value
+
+    def __eq__(*args):
+        return eq(*args, "value")
+
+    def __str__(self):
+        return f"percentile({self.value})"
---
+++ tests/test_representation.py
@@ -0,0 +1,159 @@
+import pytest
+from cf_cell_methods.representation import (
+    eq,
+    CellMethod,
+    ExtraInfo,
+    SxiInterval,
+    Percentile,
+)
+
+
+class Thing:
+    """Class for testing `eq`"""
+
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+
+@pytest.mark.parametrize(
+    "a, b, attrs, expected",
+    (
+        (Thing(1, 2, 3), Thing(1, 2, 3), "x", True),
+        (Thing(1, 2, 3), Thing(1, 2, 3), "x, y", True),
+        (Thing(1, 2, 3), Thing(1, 2, 3), "x, y, z", True),
+        (Thing(1, 2, 3), Thing(1, 2, 4), "x", True),
+        (Thing(1, 2, 3), Thing(1, 2, 4), "x, y", True),
+        (Thing(1, 2, 3), Thing(1, 2, 4), "x, y, z", False),
+    ),
+)
+def test_eq(a, b, attrs, expected):
+    assert eq(a, b, attrs) is expected
+
+
+@pytest.mark.parametrize(
+    "a, b, equal",
+    (
+        (Percentile(5), Percentile(5), True),
+        (Percentile(5), Percentile(9), False),
+        (SxiInterval("1", "year"), SxiInterval("1", "year"), True),
+        (SxiInterval("2", "year"), SxiInterval("1", "year"), False),
+        (SxiInterval("1", "day"), SxiInterval("1", "year"), False),
+        (
+            ExtraInfo(SxiInterval("1", "year"), "foo"),
+            ExtraInfo(SxiInterval("1", "year"), "foo"),
+            True,
+        ),
+        (
+            ExtraInfo(SxiInterval("1", "year"), "foo"),
+            ExtraInfo(SxiInterval("2", "year"), "foo"),
+            False,
+        ),
+        (
+            ExtraInfo(SxiInterval("1", "year"), "foo"),
+            ExtraInfo(SxiInterval("1", "year"), "bar"),
+            False,
+        ),
+        (CellMethod("time", "mean"), CellMethod("time", "mean"), True),
+        (CellMethod("time", "median"), CellMethod("time", "mean"), False),
+        (
+            CellMethod("time", "mean", where="foo"),
+            CellMethod("time", "mean", where="foo"),
+            True,
+        ),
+        (
+            CellMethod("time", "mean", where="foo"),
+            CellMethod("time", "mean", where="bar"),
+            False,
+        ),
+        (
+            CellMethod("time", "mean", over="foo"),
+            CellMethod("time", "mean", over="foo"),
+            True,
+        ),
+        (
+            CellMethod("time", "mean", over="foo"),
+            CellMethod("time", "mean", over="bar"),
+            False,
+        ),
+        (
+            CellMethod(
+                "time",
+                "mean",
+                extra_info=ExtraInfo(SxiInterval("1", "year"), "foo"),
+            ),
+            CellMethod(
+                "time",
+                "mean",
+                extra_info=ExtraInfo(SxiInterval("1", "year"), "foo"),
+            ),
+            True,
+        ),
+        (
+            CellMethod(
+                "time",
+                "mean",
+                extra_info=ExtraInfo(SxiInterval("1", "year"), "foo"),
+            ),
+            CellMethod(
+                "time",
+                "mean",
+                extra_info=ExtraInfo(SxiInterval("2", "year"), "foo"),
+            ),
+            False,
+        ),
+        (
+            [
+                CellMethod(
+                    "time",
+                    "mean",
+                    extra_info=ExtraInfo(SxiInterval("1", "year"), "foo"),
+                )
+            ],
+            [
+                CellMethod(
+                    "time",
+                    "mean",
+                    extra_info=ExtraInfo(SxiInterval("1", "year"), "foo"),
+                )
+            ],
+            True,
+        ),
+    ),
+)
+def test__eq__(a, b, equal):
+    assert (a == b) is equal
+
+
+@pytest.mark.parametrize(
+    "data, expected",
+    (
+        (Percentile(5), "percentile(5)"),
+        (SxiInterval("1", "year"), "interval: 1 year"),
+        (ExtraInfo(None, None), ""),
+        (ExtraInfo(SxiInterval("1", "year"), None), "(interval: 1 year)"),
+        (ExtraInfo(None, "this is a comment"), "(this is a comment)"),
+        (
+            ExtraInfo(SxiInterval("1", "year"), "this is a comment"),
+            "(interval: 1 year comment: this is a comment)",
+        ),
+        (CellMethod("time", "mean"), "time: mean"),
+        (CellMethod("time", "mean", "land"), "time: mean where land"),
+        (CellMethod("time", "mean", None, "years"), "time: mean over years"),
+        (
+            CellMethod("time", "mean", "land", "years"),
+            "time: mean where land over years",
+        ),
+        (
+            CellMethod(
+                "time",
+                "mean",
+                extra_info=ExtraInfo(SxiInterval("1", "year"), "wow"),
+            ),
+            "time: mean (interval: 1 year comment: wow)",
+        ),
+    ),
+)
+def test__str__(data, expected):
+    assert str(data) == expected
